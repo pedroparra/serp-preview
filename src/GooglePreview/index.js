43aafee219bbb82f515a3c50fcba8e26bb2a92ca
Oscar Otero <oom@oscarotero.com>
2020-03-26 19:27:48 +0100

    upgrade project code

---
+++ src/GooglePreview/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import styled from 'styled-components'
+import {default as PT} from 'prop-types'
+
+const Wrapper = styled.div`
+  position: relative;
+  font-family: arial, sans-serif;
+  text-align: left;
+  max-width: 600px;
+`
+const Title = styled.h3`
+  display: inline-block;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 1.3;
+  margin: 0 0 3px 0;
+  padding: 4px 0 0;
+`
+const Description = styled.div`
+  color: #3C4043;
+  line-height: 1.58;
+  font-size: 14px;
+  word-wrap: break-word;
+
+  & em {
+    font-weight: bold;
+    font-style: normal;
+  }
+`
+const Url = styled.cite`
+  position: absolute;
+  left: 0;
+  top: 0;
+  padding-top: 1px;
+  padding-bottom: 1px;
+  line-height: 1.3;
+  display: inline-block;
+  font-size: 14px;
+  color: #3C4043;
+  font-style: normal;
+  -webkit-text-size-adjust: none;
+`
+const Link = styled.a`
+  text-decoration: none;
+  color: #1a0dab;
+  outline: 0;
+
+  &:hover h3 {
+    text-decoration: underline;
+  }
+`
+
+const formatUrl = (url) => url
+  .replace(/https?:\/\//, '')
+  .replace(/\//g, ' › ')
+
+const GooglePreview = ({ title, description, url, link }) =>
+  <Wrapper>
+    <Link href={link}>
+      <br />
+      <Title>{title}</Title>
+      <Url>{formatUrl(url)}</Url>
+    </Link>
+    <Description>{description}</Description>
+  </Wrapper>
+
+GooglePreview.defaultProps = {
+  description: null,
+  link: '#'
+}
+
+GooglePreview.propTypes = {
+  /** Title */
+  title: PT.string.isRequired,
+  /** Meta Description */
+  description: PT.oneOfType([
+    PT.string,
+    PT.arrayOf(PT.node),
+    PT.node
+  ]),
+  /** Url */
+  url: PT.string.isRequired,
+  /** Link for src */
+  link: PT.string
+}
+
+export default GooglePreview
---
+++ src/YahooPreview/stories.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import YahooPreview from './'
+
+const testResults = [
+  {
+    title: '🥇React – A JavaScript library for building user interfaces',
+    description: 'React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes. Declarative views make your code more predictable and easier to debug.',
+    url: 'https://reactjs.org'
+  },
+  {
+    title: 'facebook/react: A declarative, efficient, and flexible ... - GitHub',
+    description: <>React. · <em>React</em> is a JavaScript library for building user interfaces. Declarative: React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</>,
+    url: 'https://github.com/facebook/react'
+  },
+  {
+    title: 'React Native · A framework for building native apps using React',
+    description: 'A framework for building native apps using React.',
+    url: 'https://reactnative.dev'
+  }
+]
+
+storiesOf('Yahoo')
+  .add('Default', () => (
+    testResults.map(({ title, description, url }, idx) =>
+      <figure style={{marginBottom: '27px'}} key={idx}>
+        <YahooPreview title={title} description={description} url={url} />
+      </figure>
+    )
+  ))
