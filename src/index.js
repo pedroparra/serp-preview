695b4c900b4888ed9b8539d7f6a614cb82f26ace
Diego Pascual <diego@marketgoo.com>
2020-11-12 18:21:55 +0100

    feat: added google mobile and fix in Google url

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as YahooPreview } from './YahooPreview'
 export { default as GooglePreview } from './GooglePreview'
 export { default as BingPreview } from './BingPreview'
+export { default as GoogleMobilePreview } from './GoogleMobilePreview'
